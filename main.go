6642febe52821305b180cd36422bc3718d0cd7a6
willtodd03 <willtodd03@gmail.com>
2018-02-21 22:49:22 -0500

    refactored startup logic to present new possible service architecture

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/shinypotato/authentication-service/authentication"
+	"github.com/shinypotato/authentication-service/util"
 )
 
+var envSessionRegion = util.GetRequiredStringEnv("AWS_SESSION_REGION")
+
 func main() {
-	RegisterHTTPHandlers()
-	log.Println("Listening on port 3000")
-	log.Println("/authentication/signup", "[GET]")
+	// initialize and configure aws session and cognito identity provider
+	awsSession := session.Must(session.NewSession())
+	awsSession.Config.Region = &envSessionRegion
+	cognitoIdentityProvider := cognitoidentityprovider.New(awsSession)
+	// register authentication http handlers
+	authentication.RegisterHTTPHandlers(cognitoIdentityProvider)
+	// cool logging to let us know the service is listening
+	log.Println("/authentication/signup", "[POST]")
 	http.ListenAndServe(":3000", nil)
 }
