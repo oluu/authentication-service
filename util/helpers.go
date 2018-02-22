f49d5ebe06c887b6c582f8e19860b33d735fbc0f
willtodd03 <willtodd03@gmail.com>
2018-02-21 22:50:31 -0500

    added new helpers

--- util/helpers.go
+++ util/helpers.go
@@ -1,13 +1,42 @@
 package util
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 // ReadJSON takes in a request and decodes the request into the specified interface
-func ReadJSON(r *http.Request, data interface{}) (interface{}, error) {
+func ReadJSON(r *http.Request, data interface{}) error {
 	defer r.Body.Close()
 	err := json.NewDecoder(r.Body).Decode(data)
-	return data, err
+	return err
+}
+
+// WriteResponse handles writing status code and bytes
+func WriteResponse(w http.ResponseWriter, response interface{}, statusCode int) {
+	jsonResponse, _ := json.Marshal(response)
+	w.WriteHeader(statusCode)
+	w.Write(jsonResponse)
+}
+
+// GenerateSecretHash takes in username, clientID, and secret and hashes it
+func GenerateSecretHash(username, clientID, secret string) string {
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write([]byte(fmt.Sprintf("%s%s", username, clientID)))
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+// GetRequiredStringEnv will attempt to get an environment variable of type string, if it's nil it will panic
+func GetRequiredStringEnv(key string) string {
+	env := os.Getenv(key)
+	if env == "" {
+		log.Fatalf("%s is a required environment variable", key)
+	}
+	return env
 }
