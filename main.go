25599a70462bfd5dce833fbd98c6f32cefb16d62
willtodd03 <willtodd03@gmail.com>
2018-02-22 00:49:48 -0500

    removed aws dependencies here, no external dependencies in main

--- main.go
+++ main.go
@@ -4,21 +4,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/shinypotato/authentication-service/authentication"
-	"github.com/shinypotato/authentication-service/util"
+	"github.com/shinypotato/authentication-service/identity"
 )
 
-var envSessionRegion = util.GetRequiredStringEnv("AWS_SESSION_REGION")
-
 func main() {
-	// initialize and configure aws session and cognito identity provider
-	awsSession := session.Must(session.NewSession())
-	awsSession.Config.Region = &envSessionRegion
-	cognitoIdentityProvider := cognitoidentityprovider.New(awsSession)
 	// register authentication http handlers
-	authentication.RegisterHTTPHandlers(cognitoIdentityProvider)
+	authentication.RegisterHTTPHandlers(identity.NewIdentityProvider())
 	// cool logging to let us know the service is listening
 	log.Println("/authentication/signup", "[POST]")
 	http.ListenAndServe(":3000", nil)
