cd1afda4e2ee3172fbadd6c7420b3eea7aefd3ad
willtodd03 <willtodd03@gmail.com>
2018-02-21 22:52:33 -0500

    vendor updates

--- vendor/github.com/aws/aws-sdk-go/service/sts/customizations.go
+++ vendor/github.com/aws/aws-sdk-go/service/sts/customizations.go
@@ -1,11 +1,11 @@
-package cognitoidentity
+package sts
 
 import "github.com/aws/aws-sdk-go/aws/request"
 
 func init() {
 	initRequest = func(r *request.Request) {
 		switch r.Operation.Name {
-		case opGetOpenIdToken, opGetId, opGetCredentialsForIdentity:
+		case opAssumeRoleWithSAML, opAssumeRoleWithWebIdentity:
 			r.Handlers.Sign.Clear() // these operations are unsigned
 		}
 	}
