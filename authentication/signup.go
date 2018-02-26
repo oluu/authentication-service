75bc04031d71e7e46381da408d9085bd86403520
willtodd03 <willtodd03@gmail.com>
2018-02-26 01:30:15 -0500

    add rest of the logic to signup and authenticate a new user

--- authentication/signup.go
+++ authentication/signup.go
@@ -1,18 +1,14 @@
 package authentication
 
 import (
+	"log"
+
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/shinypotato/authentication-service/util"
 )
 
-var (
-	emailStr        = "email"
-	envClientID     = util.GetRequiredStringEnv("AWS_COGNITO_CLIENT_ID")
-	envClientSecret = util.GetRequiredStringEnv("AWS_COGNITO_CLIENT_SECRET")
-)
-
 // Signup creates the user in aws cognito and authenticates them
-func Signup(signupRequest *SignupRequest, identityProvider *cognitoidentityprovider.CognitoIdentityProvider) (*cognitoidentityprovider.SignUpOutput, error) {
+func Signup(signupRequest *SignupRequest, identityProvider *cognitoidentityprovider.CognitoIdentityProvider) (*cognitoidentityprovider.InitiateAuthOutput, error) {
 	// get data required for cognito request
 	secretHash := util.GenerateSecretHash(*signupRequest.Username, envClientID, envClientSecret)
 	userAttributes := []*cognitoidentityprovider.AttributeType{
@@ -26,6 +22,33 @@ func Signup(signupRequest *SignupRequest, identityProvider *cognitoidentityprovi
 		SecretHash:     &secretHash,
 		UserAttributes: userAttributes,
 	}
-	// make request to cognito
-	return identityProvider.SignUp(cognitoSignupRequest)
+	// make request to cognito to create user
+	_, err := identityProvider.SignUp(cognitoSignupRequest)
+	if err != nil {
+		log.Println("failed to signup: ", err)
+		return nil, err
+	}
+	// make request to cognito to go ahead and verify user
+	adminConfirmRequest := &cognitoidentityprovider.AdminConfirmSignUpInput{
+		UserPoolId: &envUserPoolID,
+		Username:   signupRequest.Username,
+	}
+	_, err = identityProvider.AdminConfirmSignUp(adminConfirmRequest)
+	if err != nil {
+		log.Println("failed to confirm signup: ", err)
+		return nil, err
+	}
+	// initialize authentication parameters
+	authParameters := make(map[string]*string)
+	authParameters[authParamUsername] = signupRequest.Username
+	authParameters[authParamPassword] = signupRequest.Password
+	authParameters[authSecretHash] = &secretHash
+	// attempt to authenticate user after signup
+	authResponse, err := Authenticate(authParameters, identityProvider)
+	if err != nil {
+		log.Println("failed to authenticate: ", err)
+		return nil, err
+	}
+	// return authentication response
+	return authResponse, nil
 }
