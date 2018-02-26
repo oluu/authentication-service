fd43649bdd8483a16e838fdb4afa1a3c85683074
willtodd03 <willtodd03@gmail.com>
2018-02-26 01:30:45 -0500

    move all the vars and constants to types file

--- authentication/types.go
+++ authentication/types.go
@@ -1,5 +1,21 @@
 package authentication
 
+import "github.com/shinypotato/authentication-service/util"
+
+const (
+	authParamUsername = "USERNAME"
+	authParamPassword = "PASSWORD"
+	authSecretHash    = "SECRET_HASH"
+)
+
+var (
+	emailStr        = "email"
+	authFlow        = "USER_PASSWORD_AUTH"
+	envUserPoolID   = util.GetRequiredStringEnv("AWS_USER_POOL_ID")
+	envClientID     = util.GetRequiredStringEnv("AWS_COGNITO_CLIENT_ID")
+	envClientSecret = util.GetRequiredStringEnv("AWS_COGNITO_CLIENT_SECRET")
+)
+
 // SignupRequest is the object that is consumed from POST /authentication/signup
 type SignupRequest struct {
 	Username *string `json:"username"`
